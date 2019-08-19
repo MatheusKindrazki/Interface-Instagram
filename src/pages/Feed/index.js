41f54e6d8378bcc150248f519b0b6e6395e7153a
Matheus Kindrazki <matheus@mentores.com.br>
2019-08-19 19:16:30 -0300

    pull to refresh api

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,29 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { View, FlatList } from 'react-native';
 
-import { Post, Header, Avatar, Name, PostImage, Description } from './styles';
+import { Post, Header, Avatar, Name, PostImage, Description, Loading } from './styles';
 
 export default function Feed() {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function loadPage(pageNumber = page, shouldRefreshing = false) {
+    if(total && pageNumber > total) return ;
+
+    setLoading(true);
+    
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
+      );
+      
+      const data = await response.json();
+      const totalPost = response.headers.get('X-Total-Count');
+      
+      setTotal(Math.floor(totalPost / 5));
+      setFeed(shouldRefreshing ? data : [...feed, ...data]);
+      setPage(pageNumber + 1);
+
+      setLoading(false);
+  }
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        'http://localhost:3000/feed?_expand=author&_limit=5&_page=1'
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
-    }
-
-    loadFeed();
+    loadPage(); 
   }, [])
+
+  async function refreshList() {
+    setRefreshing(true);
+
+    await loadPage(1, true);
+
+    setRefreshing(false);
+  }
+
   return (
     <View>
       <FlatList
         data={feed}
         keyExtractor={post => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        ListFooterComponent={loading && <Loading />}
         renderItem={({ item }) => (
           <Post>
             <Header>
